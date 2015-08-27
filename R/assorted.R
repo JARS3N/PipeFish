b9edc5b3f2367ae60bb4bc8e32353a417d0b3c05
Jars3n <arsenault.jon@gmail.com>
2015-08-27 15:11:08 -0400

    again

---
+++ R/assorted.R
@@ -0,0 +1,39 @@
+
+
+checkforpackage<-function(A){y<-installed.packages()[,1];chk<-(A %in% y);if (chk==FALSE){install.packages(A,repos='http://cran.us.r-project.org',dependencies=TRUE) }}
+gradeOL<-function(u){c("A","B","C","D")[c(u < 5,u >=5 & u <10,u >=10 & u <20,u >= 20)]}
+TC<-function(x){
+  cumdif.counts<-diff(x$counts)
+  injection<-x$Time[which(cumdif.counts==min(cumdif.counts))-1]
+  period<-injection + 30
+  start<-x$counts[x$Time==x$Time[which(abs(x$Time-injection)==min(abs(x$Time-injection)))]]
+  endP<-x$counts[x$Time==x$Time[which(abs(x$Time-period)==min(abs(x$Time-period)))]]
+  endT<-min(sapply(which(abs(x$Time-period)==min(abs(x$Time-period))):(length(x$Time)-4),function(u){mean(x$counts[u:u+4])})) #avg of 5 points
+  deltaP<-start-endP
+  deltaT<-start-endT
+  abs(period/(log(1/(1-(deltaP/deltaT)))))
+}
+
+pO2<-function(TC,atm=760){
+  HC<-function(TC){(-0.0000058333*TC^3+0.0001821*TC^2+0.072405*TC+2.5443)*10000}
+  vp<-function(TC){0.0456*TC^2-0.8559*TC+16.509}
+  DO<-function(TC,vp,ap=atm){
+    if(TC>=0 & TC<30){coef<-.678;adj<-35}else{if(TC>=30 & TC<=50){coef<-.827;adj<-49}}
+    ((ap-vp)*coef)/(adj+TC)
+  }
+  DO(TC,vp(TC),atm)*(1/1000)*(1/32)*(18/1000)*HC(TC)*atm
+}
+
+
+OLgrb<-function(u){
+  import(file=u,sheet='Level') %>%
+    select(Well,pH,O2=contains("O2 (mmHg)" )) %>%
+    mutate(pHdif=abs(pH-7.4),O2dif=abs(O2-152))  %>%
+    group_by(Well) %>%
+    summarise(mxO2=max(O2dif)) %>%
+    mutate(grade=sapply(mxO2,gradeOL)) %>%
+    mutate(Lot=import(u,sheet='Assay Configuration')[27,2]) %>%
+    mutate(sn=import(u,sheet='Assay Configuration')[26,2]) %>%
+    mutate(Instrument=import(u,sheet='Assay Configuration')[35,2]) %>%
+    mutate(fl=u)
+}
---
+++ R/pKa.R
@@ -0,0 +1,73 @@
+tickfilter.A<-function(u){c(max(u),max(u)-1,max(u)-2)}
+tickfilter.B<-function(u){c(floor(median(u))-3,floor(median(u))-2,floor(median(u))-1,max(u)-2,max(u)-1,max(u))}
+##################
+grab96e<-function(u){
+#u is full file name#plat is platform type
+  pH=c(rep(3.8,3),rep(5,3),rep(5.8,3),rep(6.6,3),rep(7.0,3),rep(7.4,3),rep(8.15,3),rep(9.2,3))
+    import(u,sheet="Level") %>%
+    mutate(fl=u) %>%
+    filter(Tick %in% tickfilter.A(Tick)) %>%
+    select(counts=contains(X$counts),Tick,Well,fl) %>%
+    mutate(pH=pH[as.numeric(factor(Well))]) %>%
+    (function(u){merge(u,data.frame(dye=c(rep('CL',6),rep('PR',6)),Well=unique(u$Well)),by="Well")}) %>%
+    group_by(Well,pH,dye,fl) %>% summarise(counts=mean(counts))
+}
+#####################
+grabXFp<-function(u){
+  pH=c(3.8,5,5.8,6.6,7.0,7.4,8.15,9.2)
+  import(u,sheet="Level") %>%
+    mutate(fl=u) %>%
+    select(counts=contains("pH Corrected Em."),Tick,Well,fl)%>%
+    filter(Tick %in% tickfilter.B(Tick)) %>%
+    mutate(Tick=as.numeric(factor(Tick))) %>%
+    mutate(dye = c("CL","PR")[Tick],pH=pH[as.numeric(factor(Well))]) %>%
+    group_by(Well,pH,dye,fl) %>% summarise(counts=mean(counts))
+}
+
+################################
+grab24e<-function(u){
+  pH=c(rep(3.8,3),rep(5,3),rep(5.8,3),rep(6.6,3),rep(7.0,3),rep(7.4,3),rep(8.15,3),rep(9.2,3))
+  import(u,sheet="Level") %>%
+    mutate(fl=u) %>%
+    select(counts=contains("pH Corrected Em."),Tick,Well,fl)%>%
+    filter(Tick %in% tickfilter.B(Tick)) %>%
+    mutate(Tick=as.numeric(factor(Tick))) %>%
+    mutate(dye = c("CL","PR")[Tick],pH=pH[as.numeric(factor(Well))]) %>%
+    group_by(Well,pH,dye,fl) %>% summarise(counts=mean(counts))
+}
+###############
+ grabXF24<-function(u){
+   pH=c(rep(3.8,3),rep(5,3),rep(5.8,3),rep(6.6,3),rep(7.0,3),rep(7.4,3),rep(8.15,3),rep(9.2,3))
+   import(u,sheet="Levels",startRow = 12,readxl = FALSE,skipEmptyRows=FALSE) %>%
+    mutate(fl=u) %>%
+   select(counts=contains("pH.Cor..Em." ),Tick,Well,fl) %>%
+   filter(Tick %in% tickfilter.B(Tick)) %>%
+   mutate(Tick=as.numeric(factor(Tick))) %>%
+   mutate(dye = c(rep("CL",3),rep("PR",3))[Tick]) %>%
+   mutate(pH=pH[as.numeric(factor(Well))]) %>%
+   group_by(Well,pH,dye,fl) %>% summarise(counts=mean(counts))
+ }
+##################
+mungelist<-list(grab24e,grab96e,grabXF24,grabXFp)
+####function to run pKa
+pKa<-function(pHFluor,MFBatch,Platform,Directory){
+list.files(path=Platform,pattern='xlsx',full.names = TRUE)   %>%
+lapply(mungelist[[Platform]]) %>% Reduce(f='rbind') %>%
+write.csv(file=paste0("Directory","/","data.csv"),row.names=F)
+createRmd(pHFluor,MFBatch,Directory) %>%
+write(file=paste0("Directory","/",pHFluor,"pKa.Rmd"))
+knit(paste0("Directory","/",pHFluor,"pKa.Rmd"))
+pandoc(paste0("Directory","/",pHFluor,"pKa.md"),format="latex")
+knit2html(paste0("Directory","/",pHFluor,"pKa.md"))
+}
+createRmd<-function(pHFluor,MFBatch,Directory){
+pKaRmd<-readLines(system.file("rmd/pKaTemplate.Rmd", package="pipefish"))
+pKaRmd[15]<-gsub('data.csv',paste0("Directory","/","data.csv"),pKaRmd[15])
+pKaRmd[6]<-gsub('XBATCHX',MFBatch,pKaRmd[6])
+pKaRmd[4]<-gsub('XLOTX',pHFluor,pKaRmd[4])
+pKaRmd[43]<-gsub('pKA',paste0('pKa for ',pHFluor),pKaRmd[43])
+}
+
+
+
+
