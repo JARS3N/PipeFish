7f5c030f6a0702e9613ca28c8b4c646fbf8098f0
JARS3N <JARS3N@users.noreply.github.com>
2016-01-19 10:05:58 -0500

    Update OUTLIER.R

--- R/OUTLIER.R
+++ R/OUTLIER.R
@@ -1,30 +1,36 @@
 gradeOL<-function(u){c("A","B","C","D")[c(u < 5,u >=5 & u <10,u >=10 & u <20,u >= 20)]}
 
 OLgrb<-function(u){
-    #Import sheets from XLSX file
-    X<-list(LVL=import(file=u,sheet='Level') ,
-            AC=import(u,sheet='Assay Configuration'))
-    # O2 Outliers
-    O2 <-select(X$LVL,O2=contains("O2 (mmHg)" ),Well) %>%
-        mutate(.,O2dif=abs(O2-152))  %>%
-        group_by(.,Well) %>%
-        summarize(.,mxO2=max(O2dif)) %>%
-        mutate(.,grade=sapply(mxO2,gradeOL))
+ #Import sheets from XLSX file
+X<-list(LVL=import(file=u,sheet='Level') ,
+ AC=import(u,sheet='Assay Configuration'))
+# O2 Outliers
+ O2 <-select(X$LVL,O2=contains("O2 (mmHg)" ),Well) %>%
+       mutate(.,O2dif=abs(O2-152))  %>%
+      group_by(.,Well) %>%
+      filter(.,O2dif==max(O2dif)) %>%
+     slice(.,1) %>%
+       ungroup(.) %>%
+        rename(.,mxO2=O2dif) %>%
+       mutate(.,grade=sapply(mxO2,gradeOL))
     #pH Outliers
-      pH <- select(X$LVL, Well, pH) %>%
-        mutate(., pHdif = abs(pH -  7.4)) %>%
-        group_by(., Well) %>% 
-        summarise(., pHdif = max(pHdif)) 
-    # Tick zero median
-
-    T0 =  select(X$LVL,O2=contains("O2 (mmHg)" ),Well,Tick) %>%
-        filter(.,Tick==min(Tick)) %>%
-        summarize(med=median(O2))
-    # Final merged output
-    merge(O2,pH,by='Well') %>%
-        mutate(.,Lot=X$AC[27,2]) %>%
-        mutate(.,sn=X$AC[26,2]) %>%
-        mutate(.,Instrument=X$AC[35,2])%>%
-        mutate(.,fl=u)  %>%
-        mutate(., MedianFirstTick = T0$med )
-}
+pH<-select(X$LVL,Well,pH) %>%
+mutate(.,pHdif=abs(pH-7.4))  %>%
+group_by(.,Well) %>%
+ filter(.,pHdif==max(pHdif)) %>%
+slice(.,1) %>%
+ungroup(.) 
+    
+                   # Tick zero median
+                    
+                        T0 =  select(X$LVL,O2=contains("O2 (mmHg)" ),Well,Tick) %>%
+                                filter(.,Tick==min(Tick)) %>%
+                                summarize(med=median(O2))
+                        # Final merged output
+                            merge(O2,pH,by='Well') %>%
+                                mutate(.,Lot=X$AC[27,2]) %>%
+                                mutate(.,sn=X$AC[26,2]) %>%
+                                mutate(.,Instrument=X$AC[35,2])%>%
+                                mutate(.,fl=u)  %>%
+                                mutate(., MedianFirstTick = T0$med )
+                    }
