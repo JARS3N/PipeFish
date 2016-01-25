cd2ecffff14f26b832716508af6053171ff957f7
JARS3N <JARS3N@users.noreply.github.com>
2016-01-25 15:01:59 -0500

    Update server.R

--- inst/UploadCC/server.R
+++ inst/UploadCC/server.R
@@ -3,31 +3,32 @@ library(RMySQL)
 library(PipeFish)
 library(dplyr)
 library(XML)
-ConnectInfo<-readRDS(file.path((.libPaths()),"DataStash","DBinfo.RDS"))
-
-my_db <- dbConnect(RMySQL::MySQL(),
-                   dbname=ConnectInfo["dbname"],
-                   user=ConnectInfo["user"],
-                   password=ConnectInfo["password"],
-                   host=ConnectInfo["host"],
-                   port=as.numeric(ConnectInfo["port"]))
 
 shinyServer(function(input, output) {
     observe({
+        output$MSG <- renderText("Starting")
         if(input$goButton > 0){
+            output$MSG <- renderText("Select Directory")
             DIR<-choose.dir();
+            output$MSG <- renderText("Munging Data...")
            DF<- DIR %>%
                 list.files(path=.,pattern='asyr',full.names=TRUE) %>%
                 lapply(.,XML::xmlTreeParse) %>%
                 lapply(.,PipeFish::Collect) %>%
                 lapply(.,PipeFish::ComboAssay) %>%
-                rbind_all(.)
-
-            dbWriteTable(my_db, name="xfpwetqc", value=DF,append=TRUE,
-                         overwrite = FALSE,row.names=FALSE)
+                rbind_all(.) %>%
+               as.data.frame(.)
+           output$MSG <- renderText("Communicating with Database")
+           ConnectInfo<-readRDS(file.path((.libPaths()),"DataStash","DBinfo.RDS"))
+           my_db <- dbConnect(RMySQL::MySQL(),
+                              dbname=ConnectInfo["dbname"],
+                              user=ConnectInfo["user"],
+                              password=ConnectInfo["password"],
+                              host=ConnectInfo["host"],
+                              port=as.numeric(ConnectInfo["port"]))
+           output$MSG <- renderText("Writing to Database")
+            dbWriteTable(my_db, name="xfpwetqc", value=DF,append=TRUE,overwrite = FALSE,row.names=FALSE)
             dbDisconnect(my_db)
-
-
-
+            output$MSG <- renderText("Complete")
             }})
 })
