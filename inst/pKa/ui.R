4e6f2ff3106d21b41d77c243d8741aac7b4d3e60
JARS3N <JARS3N@users.noreply.github.com>
2016-04-06 11:19:53 -0400

    Create ui.R

---
+++ inst/pKa/ui.R
@@ -0,0 +1,21 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("PipeFish::pKa"),
+  br(),
+  mainPanel(
+    textInput('pHFluor',"pH Fluorophore", value = "Fluorophore_Validation_pKa", width = NULL),
+    textInput('MFBatch',"Multi Fluor Batch", value = "NA", width = NULL),
+    selectInput("Platform", label = "Platform", 
+                choices = list("XFe24" = 1, "XFe96" = 2, "XF24" = 3,"XFp"=4), 
+                selected = 1),
+    checkboxInput("CB", label = "Export from .Asyr", value = FALSE),
+    br(),
+    actionButton("BB","Run Analysis"),
+    br(),
+    textOutput("session"),
+    tableOutput("test1")
+  )
+  
+)
+)
