2342ba86705026783132f6ec07b8876956c96c14
JARS3N <JARS3N@users.noreply.github.com>
2016-05-10 12:24:21 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -10,15 +10,9 @@ CheckforDataStash<-function(){
   }
 
 CacheDBinfo<-function(A,B,C,D,E){
-ConnectInfo<-c( 'dbname'= A,
-                'user'= B,
-                'password'= C,
-                'host'= D,
-                'port'= E)
-LISTlib<-unlist(lapply(.libPaths(),function(u){all(dir.exists(file.path(u,"Datastash")))}))
-saveRDS(ConnectInfo,
-file.path((.libPaths()[LISTlib]),
-"DataStash",
-"DBinfo.RDS"))
+  ConnectInfo<-c( 'dbname'= A, 'user'= B,'password'= C,'host'= D,'port'= E)
+  LISTlib<-unlist(lapply(.libPaths(),function(u){all(dir.exists(file.path(u,"Datastash")))}))
+  savepath<-file.path((.libPaths()[LISTlib]),"DataStash","DBinfo.RDS")
+  saveRDS(ConnectInfo,savepath)
 }
 
