236cb0994c2f8ab78ca0b5b9ab7c1127ed7ee2fe
JARS3N <JARS3N@users.noreply.github.com>
2016-05-10 12:16:06 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,13 @@
 .onLoad <- function(libname = find.package("PipeFish"), pkgname = "PipeFish") {
-    
+    CheckforDataStash()
+}
+
+
 CheckforDataStash<-function(){
   LISTlib<-unlist(lapply(.libPaths(),function(u){all(dir.exists(file.path(u,"Datastash")))}))
   if (!(TRUE %in% LISTlib)){
     dir.create(file.path((.libPaths()[1]),DS)) 
   }
-}
-
 
 CacheDBinfo<-function(A,B,C,D,E){
 ConnectInfo<-c( 'dbname'= A,
