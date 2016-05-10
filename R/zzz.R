9e72495cc4eb810742ecdf356da065418f4f3912
JARS3N <JARS3N@users.noreply.github.com>
2016-05-10 12:26:46 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -2,17 +2,17 @@
     CheckforDataStash()
 }
 
-
-CheckforDataStash<-function(){
-  LISTlib<-unlist(lapply(.libPaths(),function(u){all(dir.exists(file.path(u,"Datastash")))}))
-  if (!(TRUE %in% LISTlib)){
-    dir.create(file.path((.libPaths()[1]),DS)) 
-  }
-
 CacheDBinfo<-function(A,B,C,D,E){
   ConnectInfo<-c( 'dbname'= A, 'user'= B,'password'= C,'host'= D,'port'= E)
   LISTlib<-unlist(lapply(.libPaths(),function(u){all(dir.exists(file.path(u,"Datastash")))}))
   savepath<-file.path((.libPaths()[LISTlib]),"DataStash","DBinfo.RDS")
   saveRDS(ConnectInfo,savepath)
 }
+CheckforDataStash<-function(){
+  LISTlib<-unlist(lapply(.libPaths(),function(u){all(dir.exists(file.path(u,"Datastash")))}))
+  if (!(TRUE %in% LISTlib)){
+    dir.create(file.path((.libPaths()[1]),DS)) 
+  }
+
+
 
