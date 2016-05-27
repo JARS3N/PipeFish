d6bcee8375228fb208e1341e304e80a589efc068
JARS3N <JARS3N@users.noreply.github.com>
2016-05-27 14:01:23 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -22,3 +22,22 @@ CheckforDataStash<-function(){
 LISTlib<-function(q){
 unlist(lapply(.libPaths(),function(u){all(dir.exists(file.path(u,q)))}))
 }
+
+givename<-function(u,splits=": "){
+  Q <-strsplit(u,split=splits);  
+  out<-Q[[1]][2];
+  names(out)<-Q[[1]][1];
+  out
+}
+
+autoUpGithub<-function(pack){
+ DESCfile<- system.file(package=pack,"DESCRIPTION")
+ DESC<- readLines(DESCfile)
+ LAST<-unlist(lapply(DESC,givename))
+ pkg<-gsub("https://github.com/","",LAST["URL"])
+ GETS<-paste0("https://raw.githubusercontent.com/",pkg,"/master/DESCRIPTION")
+ ongit<- gsub("Version: ","",grep("Version: ",readLines(GETS),value=T))
+ if (ongit!=LAST["Version"]){
+   devtools::install_github(pkg)
+ }
+}
