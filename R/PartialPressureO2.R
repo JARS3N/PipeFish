dc1020cb70261ad4349838501cf4e9e216121418
JARS3N <JARS3N@users.noreply.github.com>
2016-05-31 10:33:24 -0400

    Update PartialPressureO2.R

--- R/PartialPressureO2.R
+++ R/PartialPressureO2.R
@@ -1,14 +1,3 @@
-TC<-function(x){
-    cumdif.counts<-diff(x$counts)
-    injection<-x$Time[which(cumdif.counts==min(cumdif.counts))-1]
-    period<-injection + 30
-    start<-x$counts[x$Time==x$Time[which(abs(x$Time-injection)==min(abs(x$Time-injection)))]]
-    endP<-x$counts[x$Time==x$Time[which(abs(x$Time-period)==min(abs(x$Time-period)))]]
-    endT<-min(sapply(which(abs(x$Time-period)==min(abs(x$Time-period))):(length(x$Time)-4),function(u){mean(x$counts[u:u+4])})) #avg of 5 points
-    deltaP<-start-endP
-    deltaT<-start-endT
-    abs(period/(log(1/(1-(deltaP/deltaT)))))
-}
 
 pO2<-function(TC,atm=760){
     HC<-function(TC){(-0.0000058333*TC^3+0.0001821*TC^2+0.072405*TC+2.5443)*10000}
