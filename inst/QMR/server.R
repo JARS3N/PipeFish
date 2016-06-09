3035d992f14c42b9639468e41b49a03173e38532
JARS3N <JARS3N@users.noreply.github.com>
2016-06-09 12:05:37 -0400

    Update server.R

--- inst/QMR/server.R
+++ inst/QMR/server.R
@@ -8,12 +8,15 @@ shinyServer(function(input, output, session) {
 observe({
   if(input$GO > 0 ){
   x<- normalizePath(choose.files(multi=FALSE))
-  setwd(dirname(x))
-  out<-gsub(".xlsx",".rmd",basename(x))
-  readLines(file.path(system.file(package='PipeFish',path='rmd'),"QualifyingMediaRun.rmd")) %>%
-  gsub("FILE",basename(x),.) %>%
-  writeLines(text=.,con=out,sep="\n")
-  rmarkdown::render(input=out)}
+      if (grepl("[.]xlsx",basename(x)==TRUE){
+      setwd(dirname(x))
+      out<-gsub(".xlsx",".rmd",basename(x))
+      readLines(file.path(system.file(package='PipeFish',path='rmd'),"QualifyingMediaRun.rmd")) %>%
+      gsub("FILE",basename(x),.) %>%
+      writeLines(text=.,con=out,sep="\n")
+      rmarkdown::render(input=out)
+      }
+  }
 })
 
 
