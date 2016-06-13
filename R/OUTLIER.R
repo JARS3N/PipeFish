cee2566ae74f347e20ef74abfd3161fe931348d8
JARS3N <JARS3N@users.noreply.github.com>
2016-06-13 08:06:29 -0400

    add OLplots
    added outlier plots from shiny outlier app

--- R/OUTLIER.R
+++ R/OUTLIER.R
@@ -53,3 +53,37 @@ grep(pattern="~",x=.,invert=T,value=T)  %>%
 lapply(.,PipeFish::OLgrb) %>%
 dplyr::rbind_all()
 }
+
+plotOLCTG<-function(df,nameexp){
+    require(ggplot2);require(ggthemes);require(dplyr)
+    nRuns<-(length(unique(df$fl)))
+    df %>% mutate(ctg=paste0(Lot,"_",sn)) %>%
+        ggplot(aes(grade)) +
+        geom_bar(aes(fill=grade))+
+        facet_wrap(~ctg)+
+        ggtitle(paste0(nameexp,",n=",nRuns)) +
+        theme_bw()+
+        scale_fill_tableau()
+}
+
+
+plotOLAVGS <-function(df,nameexp){
+    require(ggplot2);require(ggthemes);require(dplyr)
+    nWells<-length(unique(df$Well))
+    nRuns<-(length(unique(df$fl)))
+   df%>%
+    group_by(.,grade) %>%
+    summarize(.,peraverage=round(n()/(nRuns* nWells)*100,3)) %>%
+    ggplot(.,aes(grade,peraverage)) +
+       geom_bar(aes(fill=grade),stat='identity')+
+       theme_bw()+
+       scale_fill_tableau()+
+       geom_text(aes(label =paste0(round(peraverage,3),"%"),
+                     x = grade, y = peraverage),
+                 position = position_dodge(width = 0.8), vjust = -0.6)+
+       ggtitle(paste0(nameexp,",n=",nRuns))
+}
+
+
+
+
