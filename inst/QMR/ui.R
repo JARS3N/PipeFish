52c9bf640f498c30c2e9261b384f42fd243f87da
JARS3N <JARS3N@users.noreply.github.com>
2016-06-14 08:02:06 -0400

    Update ui.R

--- inst/QMR/ui.R
+++ inst/QMR/ui.R
@@ -5,7 +5,9 @@ library(shiny)
 shinyUI(fluidPage(
 titlePanel("Qualifying Media Run for Instruments"),
 mainPanel(
- actionButton('GO','Run',icon=icon("cog",lib='glyphicon')) 
+ actionButton('GO','Run',icon=icon("cog",lib='glyphicon')) ,
+ br(),br(),br(),
+ actionButton('Quit','Quit',icon=icon('remove-sign',lib='glyphicon'))
 )
 )
 )
