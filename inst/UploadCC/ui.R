ab1efc1ce6fdd7c30287dc32027d54e5e192583a
JARS3N <JARS3N@users.noreply.github.com>
2016-06-14 07:47:49 -0400

    Update ui.R

--- inst/UploadCC/ui.R
+++ inst/UploadCC/ui.R
@@ -4,6 +4,7 @@ shinyUI(fluidPage(
   titlePanel("Upload XFp Data"),
   selectInput("PLAT", "Platform", c("Xfp"=1,"e24"=2,"e96"=3), selected = NULL, multiple = FALSE, selectize = TRUE, width = NULL, size = NULL),
   actionButton("goButton","RUN"),
+  actionButton("Quit", "Quit"))
   textOutput("MSG"),
   dataTableOutput('DF')
 ))
