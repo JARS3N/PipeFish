57658e56dbe072b58c2ad4eda93fc9e3b231fcd8
JARS3N <JARS3N@users.noreply.github.com>
2016-06-14 07:55:00 -0400

    Update ui.R

--- inst/ExportAsyrToExcel/ui.R
+++ inst/ExportAsyrToExcel/ui.R
@@ -1,14 +1,10 @@
 
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 
 shinyUI(fluidPage(
     p("Export Folder of asyr files to Excel"),mainPanel(
     actionButton("EXP", "Export"),
+    br(),br(),br(),
+    actionButton("Quit", "Quit"),
     textOutput("session"))
 ))
