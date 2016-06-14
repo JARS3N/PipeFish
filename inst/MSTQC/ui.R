f8b61f1919a802ce7107795ae552c3b906172e45
JARS3N <JARS3N@users.noreply.github.com>
2016-06-14 08:04:56 -0400

    Update ui.R

--- inst/MSTQC/ui.R
+++ inst/MSTQC/ui.R
@@ -7,6 +7,8 @@ shinyUI(fluidPage(
   checkboxInput("CB2", label = "Write data to csv", value = FALSE),
   checkboxInput("CB3", label = "Upload data to database", value = FALSE),
   actionButton("goButton","RUN"),
+  actionButton('Quit','Quit'),
+  br(),br(),br(),
   textOutput("MSG"),
   dataTableOutput('DF')
 ))
