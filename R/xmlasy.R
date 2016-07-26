3021975137701ad2fbdb6a7973ce954e1b7bdf48
JARS3N <JARS3N@users.noreply.github.com>
2016-07-26 10:23:58 -0400

    Update xmlasy.R

--- R/xmlasy.R
+++ R/xmlasy.R
@@ -1,68 +1,107 @@
 Collect<-function(X){
-    require(XML)
-    require(dplyr)
-    list(
-        CAL = CalData(X),
-        LVL = get_lvls(X),
-        PH_COEF=pH_coefs(X),
-        Inst = xmlValue(X[["doc"]][[1]][["InstrumentSerialNumber"]]),
-        sn = xmlValue(X[["doc"]][[1]][["Cartridge"]][["Serial"]]),
-        Lot = paste0(xmlValue(X[["doc"]][[1]][["Cartridge"]][["Type"]]),
-                     xmlValue(X[["doc"]][[1]][["Cartridge"]][["Lot"]])),
-        assay=determineAssay(X),
-        file=basename(X[['doc']]['file'][[1]])
-    )
+  require(XML)
+  require(dplyr)
+  list(
+    CAL = CalData(X),
+    LVL = get_lvls(X),
+    PH_COEF=pH_coefs(X),
+    Inst = getInst(X),
+    sn = getSn(X),
+    Lot = getLot(X),
+    assay=determineAssay(X),
+    file=getFile(X)
+  )
 }
 
 AnalyteIndex<-function(X){
- FND<-(xmlChildren(X[["doc"]][[1]][["AssayDataSet"]][["AnalyteCalibrationsByAnalyteName"]])) 
-    positionAnalyte<-unlist(lapply(FND,function(u){xmlValue(u[["Key"]][["string"]])}))
-    Analyte <-c(1,2) ;names(Analyte)<-positionAnalyte;
-    Analyte
-    }
+  FND<-(xmlChildren(X[["doc"]][[1]][["AssayDataSet"]][["AnalyteCalibrationsByAnalyteName"]])) 
+  positionAnalyte<-unlist(lapply(FND,function(u){xmlValue(u[["Key"]][["string"]])}))
+  Analyte <-c(1,2) ;names(Analyte)<-positionAnalyte;
+  Analyte
+}
+## Adding because of Legacy
+getFile(Z){basename(Z[['doc']]['file'][[1]])}
+getInst<-function(Z){xmlValue(Z[["doc"]][[1]][["InstrumentSerialNumber"]])}
 
 
+getSn<-function(u){
+  sn <- xmlValue(u[["doc"]][[1]][["Cartridge"]][["Serial"]])
+  x<-basename(u[['doc']]['file'][[1]])
+  regexp<-"_[0-9]{1,3}_"
+  if(length(sn)==0){
+    Lot<- unlist(regmatches(x, gregexpr(regexp, x))) 
+    Lot<-gsub("_","",Lot)
+    return(sprintf("%05d",Lot))
+  }else{
+    return(sn)}
+}
 
+getLot<-function(X){
+  Lot<-paste0(getCtgType(X),
+              xmlValue(X[["doc"]][[1]][["Cartridge"]][["Lot"]]))
+  if (length(Lot)==0){
+    regexp<-"[W,B,C,Q,T]{1}[E,0-9]{1}[0-9]{4}"
+    Lot<- unlist(regmatches(getFile(X), gregexpr(regexp, x)))
+  }
+  return(Lot)
+}
+
+getCtgTypefromInst<-function(Z){
+  Instperfix<-substr(getInst(Z),1,2)  
+  lettr<-c("Q"=10,"W"=20,"C"=43,"W"=41,"B"=42)
+  names(lettr[lettr==Instperfix])
+}
+
+getCtgType<-function(Z){
+ Ltr<- xmlValue(Z[["doc"]][[1]][["Cartridge"]][["Type"]])
+  if (length(Ltr)==0){
+  Ltr<-getCtgTypefromInst(Z)
+  }
+ Ltr
+}
+
+
+########################
 pH_coefs<-function(X){
-   Analyte<-AnalyteIndex(X)
-    coefs<-list(
+  Analyte<-AnalyteIndex(X)
+  coefs<-list(
     "slope"= as.numeric(xmlValue(X[["doc"]][[1]][["AssayDataSet"]][["AnalyteCalibrationsByAnalyteName"]][ Analyte["pH"]][["Item"]][2][["Value"]][["AnalyteCalibration"]][["GainEquation"]][["C3"]])
     ),
     "intercept"=as.numeric(xmlValue(X[["doc"]][[1]][["AssayDataSet"]][["AnalyteCalibrationsByAnalyteName"]][Analyte["pH"]][["Item"]][2][["Value"]][["AnalyteCalibration"]][["GainEquation"]][["C4"]])
     ),
     "target"=as.numeric(xmlValue(X[["doc"]][[1]][["AssayDataSet"]][["AnalyteCalibrationsByAnalyteName"]][Analyte["pH"]][["Item"]][2][["Value"]][["AnalyteCalibration"]][["TargetEmissionValue"]])
     ))
-coefs$gain<-c((coefs$slope * coefs$target)+ coefs$intercept);
-coefs
+  coefs$gain<-c((coefs$slope * coefs$target)+ coefs$intercept);
+  coefs
 }
 
 
 CalData<-function(u){
-   A<- u[["doc"]][[1]][["AssayDataSet"]][["AnalyteCalibrationsByAnalyteName"]]
-    merge(
-        unlisted(A[[1]][["Value"]][["AnalyteCalibration"]]),
-        unlisted(A[[2]][["Value"]][["AnalyteCalibration"]]),
-        by='Well')
+  A<- u[["doc"]][[1]][["AssayDataSet"]][["AnalyteCalibrationsByAnalyteName"]]
+  merge(
+    unlisted(A[[1]][["Value"]][["AnalyteCalibration"]]),
+    unlisted(A[[2]][["Value"]][["AnalyteCalibration"]]),
+    by='Well')
 }
 
 
 
 unlisted<-function(U){
-    data.frame(
-        LED= lapply(xmlChildren(U[["LedValues"]]),function(j){as.numeric(xmlSApply(j,xmlValue))}) %>%
-            Reduce(f='c',x=.),
-        CalEmission=lapply(xmlChildren(U[["CalibrationEmissionValues"]]),function(j){as.numeric(xmlSApply(j,xmlValue))}) %>%
-            Reduce(f='c',x=.),
-        Status=lapply(xmlChildren(U[["LedStatusValues"]]),function(j){(xmlSApply(j,xmlValue))}) %>%
-            Reduce(f='c',x=.)) %>%
-        setNames(paste0(xmlValue(U[["AnalyteName"]]),".",names(.))) %>%
-        mutate(Well=1:length(.[,1]))
+  data.frame(
+    LED= lapply(xmlChildren(U[["LedValues"]]),function(j){as.numeric(xmlSApply(j,xmlValue))}) %>%
+      Reduce(f='c',x=.),
+    CalEmission=lapply(xmlChildren(U[["CalibrationEmissionValues"]]),function(j){as.numeric(xmlSApply(j,xmlValue))}) %>%
+      Reduce(f='c',x=.),
+    Status=lapply(xmlChildren(U[["LedStatusValues"]]),function(j){(xmlSApply(j,xmlValue))}) %>%
+      Reduce(f='c',x=.)) %>%
+    setNames(paste0(xmlValue(U[["AnalyteName"]]),".",names(.))) %>%
+    mutate(Well=1:length(.[,1]))
 }
 
 determineAssay<-function(X){
-    basename(xmlValue(X[['doc']][[1]][['FileName']])) %>%
-        c("Gain"=grepl("GAIN",toupper(.)),"Ksv"=grepl("KSV",toupper(.)))%>%
-        (function(u){names(u[u==TRUE])})
+  basename(xmlValue(X[['doc']][[1]][['FileName']])) %>%
+    c("Gain"=grepl("GAIN",toupper(.)),"Ksv"=grepl("KSV",toupper(.)))%>%
+    (function(u){names(u[u==TRUE])})
 }
 
 
@@ -85,59 +124,59 @@ get_lvls<-function(u){
 
 
 TickTable<-function(u){
- Q<-u[['doc']][[1]][["AssayDataSet"]][["RateSpans"]]
-    bind_rows(lapply(1:length(Q),function(i){
-               qfrom=as.numeric(xmlValue(Q[[i]][["StartTickIndex"]]));
-               qto=as.numeric(xmlValue(Q[[i]][["EndTickIndex"]]));
-               Measure<-i;
-               data.frame(Tick=seq(from=qfrom,to=qto,by=1),Measure=Measure)})
-    )
+  Q<-u[['doc']][[1]][["AssayDataSet"]][["RateSpans"]]
+  bind_rows(lapply(1:length(Q),function(i){
+    qfrom=as.numeric(xmlValue(Q[[i]][["StartTickIndex"]]));
+    qto=as.numeric(xmlValue(Q[[i]][["EndTickIndex"]]));
+    Measure<-i;
+    data.frame(Tick=seq(from=qfrom,to=qto,by=1),Measure=Measure)})
+  )
 }
 
 
 
 ComboAssay<-function(X){
-    require(tidyr)
-    Dat<-  list(
-        pHgain=filter(X$LVL,Measure==1)%>%
-            filter(.,Tick %in% c(max(Tick)-2,max(Tick)-1,max(Tick))) %>%
-            group_by(.,Well) %>% summarise(.,sorpH=mean(pHlvl)) %>%
-            merge(.,X$CAL,by='Well') %>%mutate(.,Target=X$PH_COEF$target) %>%
-            mutate(.,Gain=(Target/pH.CalEmission)*(1/800)*(pH.CalEmission-sorpH)),
-        O2gain=group_by(X$LVL,Measure) %>%
-            filter(.,Tick %in% c(max(Tick)-2,max(Tick)-1,max(Tick))) %>%
-            ungroup(.) %>%
-            group_by(.,Well,Measure) %>% summarise(.,avgO2lvl=mean(O2lvl)) %>%
-            mutate(.,Measure=c("Ambient","F0")[Measure])%>%
-            spread(.,Measure,avgO2lvl) %>%
-            mutate(.,KSV=((F0/Ambient)-1)/152) %>%
-            ungroup(.) %>%
-            mutate(Well=as.numeric(Well))
-    )
-    merge(Dat$pHgain,Dat$O2gain,by='Well') %>%
-        mutate(.,inst=as.character(X$Inst))%>%
-        mutate(.,sn=as.character(X$sn)) %>%
-        mutate(.,Lot=as.character(X$Lot))
+  require(tidyr)
+  Dat<-  list(
+    pHgain=filter(X$LVL,Measure==1)%>%
+      filter(.,Tick %in% c(max(Tick)-2,max(Tick)-1,max(Tick))) %>%
+      group_by(.,Well) %>% summarise(.,sorpH=mean(pHlvl)) %>%
+      merge(.,X$CAL,by='Well') %>%mutate(.,Target=X$PH_COEF$target) %>%
+      mutate(.,Gain=(Target/pH.CalEmission)*(1/800)*(pH.CalEmission-sorpH)),
+    O2gain=group_by(X$LVL,Measure) %>%
+      filter(.,Tick %in% c(max(Tick)-2,max(Tick)-1,max(Tick))) %>%
+      ungroup(.) %>%
+      group_by(.,Well,Measure) %>% summarise(.,avgO2lvl=mean(O2lvl)) %>%
+      mutate(.,Measure=c("Ambient","F0")[Measure])%>%
+      spread(.,Measure,avgO2lvl) %>%
+      mutate(.,KSV=((F0/Ambient)-1)/152) %>%
+      ungroup(.) %>%
+      mutate(Well=as.numeric(Well))
+  )
+  merge(Dat$pHgain,Dat$O2gain,by='Well') %>%
+    mutate(.,inst=as.character(X$Inst))%>%
+    mutate(.,sn=as.character(X$sn)) %>%
+    mutate(.,Lot=as.character(X$Lot))
 }
 
 
 
 newGain<-function(X){
-    filter(X$LVL,Tick %in% c(max(Tick)-2,max(Tick)-1,max(Tick))) %>%
-        group_by(.,Well) %>% summarise(.,sorpH=mean(pHlvl)) %>%
-        merge(.,X$CAL,by='Well') %>%mutate(.,Target=X$PH_COEF$target) %>%
-        mutate(.,Gain=(Target/pH.CalEmission)*(1/800)*(pH.CalEmission-sorpH))
+  filter(X$LVL,Tick %in% c(max(Tick)-2,max(Tick)-1,max(Tick))) %>%
+    group_by(.,Well) %>% summarise(.,sorpH=mean(pHlvl)) %>%
+    merge(.,X$CAL,by='Well') %>%mutate(.,Target=X$PH_COEF$target) %>%
+    mutate(.,Gain=(Target/pH.CalEmission)*(1/800)*(pH.CalEmission-sorpH))
 }
 
 Ksv<-function(X){
-    group_by(X$LVL,Measure) %>%
-        filter(.,Tick %in% c(max(Tick)-2,max(Tick)-1,max(Tick))) %>%
-        ungroup(.) %>%
-        group_by(.,Well,Measure) %>% summarise(.,avgO2lvl=mean(O2lvl)) %>%
-        mutate(.,Measure=c("Ambient","F0")[Measure])%>%
-        tidyr::spread(.,Measure,avgO2lvl) %>%
-        mutate(.,KSV=((F0/Ambient)-1)/152) %>%
-        merge(.,X$CAL,by='Well')
+  group_by(X$LVL,Measure) %>%
+    filter(.,Tick %in% c(max(Tick)-2,max(Tick)-1,max(Tick))) %>%
+    ungroup(.) %>%
+    group_by(.,Well,Measure) %>% summarise(.,avgO2lvl=mean(O2lvl)) %>%
+    mutate(.,Measure=c("Ambient","F0")[Measure])%>%
+    tidyr::spread(.,Measure,avgO2lvl) %>%
+    mutate(.,KSV=((F0/Ambient)-1)/152) %>%
+    merge(.,X$CAL,by='Well')
 }
 
 
