2aee074e910d43b7269476c732701950f3acd881
JARS3N <JARS3N@users.noreply.github.com>
2016-08-10 09:43:46 -0400

    Update server.R

--- inst/UploadCC/server.R
+++ inst/UploadCC/server.R
@@ -4,49 +4,30 @@ library(PipeFish)
 library(dplyr)
 library(XML)
 
+
 shinyServer(function(input, output) {
-    observeEvent(input$Quit, {
-        stopApp(returnValue = invisible())
-     })
+  observeEvent(input$Quit, {
+    stopApp(returnValue = invisible())
+  })
+  observe({
+    output$MSG <- renderText("Ready")
+    if(input$goButton > 0){
+      output$MSG <- renderText("Select Directory")
+      DIR<-choose.dir();
+      output$MSG <- renderText("Munging Data...")
 
-
-    observe({
-        output$MSG <- renderText("Ready")
-        if(input$goButton > 0){
-            output$MSG <- renderText("Select Directory")
-            DIR<-choose.dir();
-            output$MSG <- renderText("Munging Data...")
-            if(input$PLAT ==1){
-           DF<<- DIR %>%
-                list.files(path=.,pattern='asyr',full.names=TRUE) %>%
-                lapply(.,XML::xmlTreeParse) %>%
-                lapply(.,PipeFish::Collect) %>%
-                lapply(.,PipeFish::ComboAssay) %>%
-                dplyr::bind_rows(.)%>%
-               as.data.frame(.)
-               }else{
-               DF<<- DIR %>%
-                list.files(path=.,pattern='asyr',full.names=TRUE) %>%
-                lapply(.,XML::xmlTreeParse) %>%
-                lapply(.,PipeFish::Collect) %>%
-                lapply(.,PipeFish::assay) %>%
-                dplyr::bind_rows(.) %>%
-               as.data.frame(.)
-               }
-               
-               
-               output$DF<-shiny::renderDataTable(DF)
-           output$MSG <- renderText("Communicating with Database")
-           ConnectInfo<-DataStash::Triton()
-           my_db <- dbConnect(RMySQL::MySQL(),
-                              dbname=ConnectInfo[1],
-                              user=ConnectInfo[2],
-                              password=ConnectInfo[3],
-                              host=ConnectInfo[4],
-                              port=as.numeric(ConnectInfo[5]))
-           output$MSG <- renderText("Writing to Database")
-            dbWriteTable(my_db, name=c("xfpwetqc","xfe24wetqc","xfe96wetqc","xf24legacy")[as.numeric(input$PLAT)], value=DF,append=TRUE,overwrite = FALSE,row.names=FALSE)
-            dbDisconnect(my_db)
-            output$MSG <- renderText("Complete")
-            }})
+      DF<-DIR %>% 
+        list.files(path=.,pattern='asyr',full.names=TRUE) %>%
+        lapply(.,XML::xmlTreeParse) %>% 
+        lapply(.,PipeFish::Collect) %>% 
+        lapply(.,PipeFish::whichAssay) %>% 
+        dplyr::bind_rows(.) %>%
+        as.data.frame(.)
+      
+      output$DF<-shiny::renderDataTable(DF)
+      output$MSG <- renderText("Communicating with Database")
+      output$MSG <- renderText("Writing to Database")
+      PipeFish::UploadsCC(DF)
+      output$MSG <- renderText("Complete")
+    }})
 })
