2e4a163486c3e3781c3606e534543b8791ad7b67
JARS3N <JARS3N@users.noreply.github.com>
2016-10-05 16:58:59 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -46,7 +46,7 @@ autoUpGithub<-function(pack){
   }
   pkg<-packageDescription(pack)$URL
   GETS<-paste0("https://raw.githubusercontent.com/",pkg,"/master/DESCRIPTION")
-  if (testUrl(GETS)==FALSE){return(message("Can't seem to cotact github repo\n will not update."))}
+  if (testUrl(GETS)==FALSE){return(message("Can't seem to contact github repo\n will not update."))}
   ongit<- gsub("Version: ","",grep("Version: ",readLines(GETS,warn=F),value=T))
   if (ongit!=utils::packageVersion(pack)){
     message(paste0("Github version differs from installed version \n update",pack))
