6f25f726088d3daa5bd0543c380dc16abbdfda92
JARS3N <JARS3N@users.noreply.github.com>
2016-12-06 14:12:37 -0500

    Update ui.R

--- inst/Launcher/ui.R
+++ inst/Launcher/ui.R
@@ -1,18 +1,29 @@
 
 library(shiny)
 library(PipeFish)
-scriptlist<-PipeFish::available_scripts()
+#scriptlist<-PipeFish::available_scripts()
 shinyUI(
   fluidPage(title="PipeFish::Scripts",
-  fluidRow(
-    column(h4("Select Sript"),width=4,
-selectInput(inputId="Scripts",label="",choices=scriptlist,multiple=FALSE,selected=NULL)
-          ),#col select
-    column(width=5,
-      actionButton(inputId="Launch",label="Launch Script", icon = icon('rocket'))
-    )
-
-),#fluidrow
-fluidRow(column(h4("Description"),width=9,p("Descriptions etc etc etc ...")))
-)#fluidpage
+            fluidRow(
+              column(h4("Select Script"),width=4,
+                     selectInput(inputId="Scripts",label="",
+                          choices=c(
+                            "WavetoExcel"=1,
+                            "XFDtoWave"=2,
+                            "Outliers"=3,
+                            "pKa(fromAsyr)"=4,
+                            "UploadDryQC"=5,
+                            "reprintBarcodes"=6
+                          )       
+                                   
+                                   
+                                   ,multiple=FALSE,selected=NULL)
+              ),#col select
+              column(width=5,
+                     actionButton(inputId="Launch",label="Launch Script", icon = icon('rocket'))
+              )
+              
+            ),#fluidrow
+            fluidRow(column(h4("Description"),width=9,textOutput("MSG")))
+  )#fluidpage
 )#ui
