6069cd67d7f4e22a4654008bcd181b15fca40cd4
JARS3N <JARS3N@users.noreply.github.com>
2016-12-12 08:13:09 -0500

    Update G2buffercapacity.R

--- R/G2buffercapacity.R
+++ R/G2buffercapacity.R
@@ -42,7 +42,64 @@ require(tidyr)
    mutate(.,bufferCapacity=molesH/(deltapH*volMedia)) %>% 
    mutate(file=fl)
 }
+###### function to use for QC
+
+
+G2BC2<-function(fl){
+  require(dplyr)
+  require(readxl)
+  require(tidyr)
+  G2F1<-{
+    . %>% 
+      readxl::read_excel(.,sheet="Raw")%>% 
+      select(.,Measurement,Tick,Well,pH) %>% 
+      # grab last 3 ticks of measure and take the mean
+      group_by(.,Measurement) %>% 
+      filter(.,Tick %in% c(max(Tick)-c(2,1,0))) %>% 
+      ungroup(.) %>% 
+      group_by(.,Measurement,Well) %>% 
+      summarize(.,meanpH=mean(pH)) %>% 
+      ungroup(.) %>% 
+      mutate(.,pHM=c("startpH","finalpH")[Measurement]) %>% 
+      select(.,-Measurement) %>% 
+      tidyr::spread(.,key=pHM,value=meanpH)
+  }
+  FUNs<-list(
+    '96'=function(u){
+      u %>%
+        mutate(.,Col=gsub("[A-Z]","",Well) %>% as.numeric()) %>% 
+        mutate(.,volHCl=rep(c(0,20,42,67),3)[Col]) %>% 
+        select(.,-Col) %>% 
+        mutate(.,deltapH=startpH-finalpH) %>% 
+        mutate(.,HClMolarity=5/1000) %>% 
+        mutate(.,molesH=(HClMolarity*(volHCl/1000^2))) %>% 
+        mutate(.,volMedia=180/(1000^2)) %>% 
+        mutate(.,bufferCapacity=molesH/(deltapH*volMedia)) 
+    },
+    '24'=function(u){
+      u %>% 
+        mutate(.,volHCl=c(rep(20,6),rep(42,6),rep(67,6),rep(0,6))) %>% 
+        mutate(.,deltapH=startpH-finalpH) %>% 
+        mutate(.,HClMolarity=5/1000) %>% 
+        mutate(.,molesH=(HClMolarity*(volHCl/1000^2))) %>% 
+        mutate(.,volMedia=620/(1000^2)) %>% 
+        mutate(.,bufferCapacity=molesH/(deltapH*volMedia)) 
+    },
+    '8'=function(u){
+      u %>%  mutate(.,volHCl=c(0,20,42,67) %>% rep(.,2)) %>% 
+        mutate(.,deltapH=startpH-finalpH) %>% 
+        mutate(.,HClMolarity=5/1000) %>% 
+        mutate(.,molesH=(HClMolarity*(volHCl/1000^2))) %>% 
+        mutate(.,volMedia=180/(1000^2)) %>% 
+        mutate(.,bufferCapacity=molesH/(deltapH*volMedia)) 
+    }
+  )
   
+  G2F1(fl) %>% 
+  {FUNs[[.$Well %>% unique() %>% length() %>% as.character()]](.)} %>% 
+    mutate(.,fl=fl)
+  
+}
   
 ###### app for G2BC
 
