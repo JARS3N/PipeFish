161659ca250e262257d6b42dad4f032180c1a42c
JARS3N <JARS3N@users.noreply.github.com>
2017-02-02 10:42:12 -0500

    Update ui.R

--- inst/UPLOADLLQC/ui.R
+++ inst/UPLOADLLQC/ui.R
@@ -9,6 +9,7 @@ shinyUI(fluidPage(
   titlePanel("PipeFish::Upload Instrument QC Light Leak Data"),
     mainPanel(
       p("Select directory containing xlsx files from light leak utility test"),
+      checkboxInput("CB", "open and save XLSX files first", value = TRUE, width = NULL)
       actionButton('send', "Upload Data", icon = icon("cog", lib = "glyphicon"), width = NULL)
     )
   )
