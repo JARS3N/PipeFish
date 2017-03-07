6e1f96e2eb6fa00cb0f9645e6ec36844aa0380e7
JARS3N <JARS3N@users.noreply.github.com>
2017-03-07 12:29:48 -0500

    Update server.R

--- inst/MSTQC/server.R
+++ inst/MSTQC/server.R
@@ -3,7 +3,7 @@ library(shiny)
 library(RMySQL)
 library(PipeFish)
 library(dplyr)
-ConnectInfo<-PipeFish::DBinfo()
+ConnectInfo<-DataStash::Triton()
 shinyServer(function(input, output,session) {
  session$onSessionEnded(function() {
     stopApp()
@@ -30,12 +30,7 @@ shinyServer(function(input, output,session) {
         if (input$CB2==TRUE){write.csv(DF,file.path(DIR,'MSToutput.csv'),row.names=FALSE)}
         if (input$CB3==TRUE){
             ConnectInfo<-DataStash::Triton()
-            my_db <- dbConnect(RMySQL::MySQL(),
-                               dbname=ConnectInfo[1],
-                               user=ConnectInfo[2],
-                               password=ConnectInfo[3],
-                               host=ConnectInfo[4],
-                               port=as.numeric(ConnectInfo[5]))
+            my_db <- rmysqlCon()
             dbWriteTable(my_db, name='mstqc', value=DF,append=TRUE,overwrite = FALSE,row.names=FALSE)
             dbDisconnect(my_db)
         }
