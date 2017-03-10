42a885e94f5eb9b5abb867bf8e62fc1c9287fdfd
JARS3N <JARS3N@users.noreply.github.com>
2017-03-10 14:54:03 -0500

    Update CalDat.R

--- R/CalDat.R
+++ R/CalDat.R
@@ -1,22 +1,46 @@
 CalDat<-function(fl=file.choose()){
+  require(dplyr)
   GetValues<-function(J){
-    lapply(seq_along(J),function(x){
-      J[[x]] %>% XML::xmlSApply(.,XML::xmlValue) %>%
-        as.numeric()}) %>% unlist()
+    Dat<-lapply(seq_along(J),function(x){
+      J[[x]] %>% XML::xmlSApply(.,XML::xmlValue)})  %>% unlist
+    if(unique(names(Dat))=="CalibrationQuality"){
+      return(Dat %>% as.character())
+    }else{
+      return(Dat %>% as.numeric())
+    }
+  }
+  getType<-function(L){
+    data.frame(n=c(8,24,96),ctype=c("C","B","W")) %>% 
+      filter(.,n==L) %>% 
+      select(ctype) %>% 
+      .$ctype %>% as.vector()
+  }
+  getWells<-function(Type){
+    list(
+    "W"= paste0(sapply(LETTERS[1:8],rep,12),sprintf("%02d",c(1:12))),
+    "B" = paste0(sapply(LETTERS[1:4],rep,6),sprintf("%02d",c(1:6))),
+    "C" = paste0(LETTERS[1:8],"01")
+    )[[Type]]
   }
   A<-XML::xmlTreeParse(fl)
-  O2<-A$doc$children$CalibrationData[["O2Data"]][["LedValues"]]
-  PH<-A$doc$children$CalibrationData[["PHData"]][["LedValues"]]
-  Well=paste0(sapply(LETTERS[1:8],rep,12),
-              sprintf("%02d",c(1:12)))
-  file<-A$doc$file
-  Lot<-XML::xmlValue(A$doc$children$CalibrationData[["CartridgeLot"]]) %>%
-    paste0("W",.)
-  sn<-XML::xmlValue(A$doc$children$CalibrationData[["CartridgeSerial"]])
 
-  data.frame(
-    Well=Well,pH.LED=GetValues(PH),O2.LED=GetValues(O2),
-    Lot=Lot,sn=sn,file=file
-
-  )
+ pullData<-function(dat){
+    A<-dat$doc$children$CalibrationData
+    analyte<-list("O2","PH")
+    vars<-list("LED"="LedValues","Status"="LedStatusValues",
+               "CalEmission"="CalibrationEmissionValues",
+               "RefDelta"="IntialReferenceDeltaValues")
+    perAnalyte<-function(An){
+      lapply(vars,{. %>% A[[paste0(An,"Data")]][[.]] %>% GetValues(.)}) %>% 
+        as.data.frame.list(.,col.names=paste(An,names(.),sep="."))
+    }
+    lapply(analyte,perAnalyte) %>% 
+      bind_cols() %>% 
+      mutate(.,type=getType(nrow(.))) %>% 
+      mutate(Lot=paste0(type,XML::xmlValue(A[["CartridgeLot"]]))) %>% 
+      mutate(sn=XML::xmlValue(A[["CartridgeSerial"]])) %>% 
+      mutate(Well=getWells(unique(type))) %>% 
+      select(.,-type)
+ }
+ pullData(A)
 }
