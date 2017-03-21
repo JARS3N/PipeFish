7ad3d2774c0166ab3627a2ef14de2ecdebc90748
JARS3N <JARS3N@users.noreply.github.com>
2017-03-21 12:46:17 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,29 +1,8 @@
 .onLoad <- function(libname = find.package("PipeFish"), pkgname = "PipeFish") {
     #CheckforDataStash();
     autoUpGithub('PipeFish')
+    checkUsr()
 }
-# REMOVED THE NEED FOR CHECK DATASTASH BY JUST CREATING A PACKAGE FOR DATASTASH,AVOIDS ALL THE OTHER MESS
-
-#CacheDBinfo<-function(A,B,C,D,E){
-#names(A)<-NULL;names(B)<-NULL;names(C)<-NULL;names(D)<-NULL;names(E)<-NULL;
-#  ConnectInfo<-c( 'dbname'= A, 'user'= B,'password'= C,'host'= D,'port'= E)
-#  savepath<-file.path(.libPaths()[LISTlib("DataStash")],"DataStash","DBinfo.RDS")
-#  saveRDS(ConnectInfo,savepath)
-#}
-#CheckforDataStash<-function(){
-#  LISTlib<-LISTlib("DataStash")
-#  if (!(TRUE %in% LISTlib)){
-#    dir.create(file.path((.libPaths()[1]),"DataStash"))
-#  }}
-
-#  DBinfo<-function(){
-#  fp<-file.path((.libPaths()[LISTlib("DataStash")]),"DataStash","DBinfo.RDS")
-#  readRDS(fp)
-#  }
-
-#LISTlib<-function(q){
-#unlist(lapply(.libPaths(),function(u){all(dir.exists(file.path(u,q)))}))
-#}
 
 givename<-function(u,splits=": "){
   Q <-strsplit(u,split=splits);
@@ -55,4 +34,51 @@ autoUpGithub<-function(pack){
 }
 
 
-wuzzy<-"fzzywzzy"
+checkUsr<-function(){
+  require(dplyr)
+  getLink<-function(){
+    getsubd<-function(u){
+      marker<-"Connection-specific DNS Suffix  . :"
+      shell("ipconfig",intern=T) %>%
+        grep(marker,.,value=T) %>%
+        gsub(marker,"",.) %>%
+        gsub(" ","",.) %>%
+        unique() %>%
+        .[.!=""]
+    }
+    
+    data.frame(user=Sys.getenv("USERNAME"),
+               comp=Sys.getenv("COMPUTERNAME"),
+               domain=getsubd()) %>% 
+      mutate(
+        link=paste0("http://",comp,".",domain,":9999"))
+  }#getLink
+  
+  addUsr<-function(info){
+    require(RMySQL)
+    DB<-PipeFish::rmysqlCon()
+    dbWriteTable(DB, name="pfusr",value=info,
+                 append=T,overwrite = F,row.names=FALSE)
+    dbDisconnect(DB)
+    saveRDS(Sys.Date(),file.path(system.file(package="door"),"completed.RDS"))
+  }#addUsr
+  
+  getUsrs<-function(){
+    db<-PipeFish::dplyrCON()
+    db %>% tbl('pfusr') %>% 
+      select(user) %>% 
+      distinct() %>% 
+      collect() %>% 
+      .$user %>%
+      unname()
+  }#getUsrs
+  
+  if(!file.exists(system.file(package="door","completed.RDS"))){
+    info<-getLink()
+    if(exists(info)){
+      all<-getUsrs()
+      if(!any(info$user %in% all)){addUsr(info)}
+    }
+  }
+}
+}
