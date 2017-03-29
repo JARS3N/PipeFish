0386229fa6c0c94de1394bd47b0dd99ec82514b3
JARS3N <JARS3N@users.noreply.github.com>
2017-03-29 14:09:44 -0400

    Create getDetailsXML.R

---
+++ R/getDetailsXML.R
@@ -0,0 +1,12 @@
+getDetailsXML<-function(DIR=choose.dir()){
+require(parallel);require(dplyr);
+require(xml2);
+FLS<- list.files(DIR,recursive=T,pattern='details.xml',full.names = T);
+size.of.list <- length(FLS);
+cl <- makeCluster( min(size.of.list, detectCores()) );
+Out<-parallel::parLapply(cl=cl,FLS,PipeFish::MVdetails) %>%
+  .[!grepl("FAILURE",.)] %>%
+  bind_rows()
+stopCluster(cl);
+Out
+}
