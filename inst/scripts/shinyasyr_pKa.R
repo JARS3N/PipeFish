4f29e0ec355f7d663082023598634257891a8be1
JARS3N <JARS3N@users.noreply.github.com>
2017-05-15 09:40:39 -0400

    Update shinyasyr_pKa.R

--- inst/scripts/shinyasyr_pKa.R
+++ inst/scripts/shinyasyr_pKa.R
@@ -1,2 +1,4 @@
 
-shiny::runApp(system.file("ASYRpKa",package="PipeFish"),launch.browser = TRUE)
+#shiny::runApp(system.file("ASYRpKa",package="PipeFish"),launch.browser = TRUE)
+
+miniUI::runGadget(system.file("ASYRpKa",package="PipeFish"), viewer = dialogViewer('thing',width=800,height=600))
